beb17bb90017ff89d728fa979ba7e9f93983905c
arashsoft <arashsoft@outlook.com>
2020-03-25 00:12:42 -0700

    start

---
+++ README.md
@@ -0,0 +1,2 @@
+
+_Note: See [How to Prevent Permissions Errors](https://docs.npmjs.com/getting-started/fixing-npm-permissions) if you are running into issues when trying to install packages globally._
---
+++ src/app/newcheck/newcheck.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//import {DpDatePickerModule} from 'ng2-jalali-date-picker';
+
+@Component({
+  selector: 'app-newcheck',
+  templateUrl: './newcheck.page.html',
+  styleUrls: ['./newcheck.page.scss'],
+})
+export class NewcheckPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
